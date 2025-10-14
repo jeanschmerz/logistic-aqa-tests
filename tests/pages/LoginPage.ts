b8a2d4f89117d05f63fd060b0d988983ee851635
Jean Schmerz <soldier.of.sixth.heaven@gmail.com>
2025-10-14 20:25:00 +0300

    upd StartPage, LoginPage

--- tests/pages/LoginPage.ts
+++ tests/pages/LoginPage.ts
@@ -1,34 +1,37 @@
-import test, { expect, Locator, Page } from '@playwright/test';
-import { credentials } from '../fixtures/testData';
+import { Locator, expect } from '@playwright/test';
+import { BasePage } from './BasePage';
 
-export class LoginPage {
-  readonly page: Page;
+export class LoginPage extends BasePage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
   readonly errorMessage: Locator;
+  readonly inventoryHeader: Locator;
 
-  constructor(page: Page) {
-    this.page = page;
-    this.usernameInput = page.locator('[data-test="username"]');
-    this.passwordInput = page.locator('[data-test="password"]');
-    this.loginButton = page.locator('[data-test="login-button"]');
-    this.errorMessage = page.locator('[data-test="error"]');
+  constructor(page: any) {
+    super(page);
+    this.usernameInput = this.page.locator('[data-test="username"]');
+    this.passwordInput = this.page.locator('[data-test="password"]');
+    this.loginButton = this.page.locator('[data-test="login-button"]');
+    this.errorMessage = this.page.locator('[data-test="error"]');
+    this.inventoryHeader = this.page.locator('[data-test="header-container"]');
   }
 
   async open() {
-    await this.page.goto('https://www.saucedemo.com/');
+    await this.navigateTo('/');
   }
+
   async login(username: string, password: string) {
     await this.usernameInput.fill(username);
     await this.passwordInput.fill(password);
     await this.loginButton.click();
   }
+
   getErrorMessage() {
-    return this.page.locator('[data-test="error"]');
+    return this.errorMessage;
   }
 
   getInventoryList() {
-    return this.page.locator('.inventory_list');
+    return this.inventoryHeader;
   }
 }
